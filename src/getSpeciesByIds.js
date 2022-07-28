4d2ec2a55aecda475689cd10add99ea2dfc8d7b2
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-28 17:44:15 -0300

    UPDATE: requirement 1 refactored

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,6 @@
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  return ids.length === 0 ? [] : ids.map((id) => species.find((specie) => specie.id === id));
-}
+const getSpeciesByIds = (...ids) => ids.map((id) => species.find((specie) => specie.id === id));
 
 module.exports = getSpeciesByIds;
