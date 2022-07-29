2664938ecc60d738dade7898c65355aad7094594
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-29 18:13:00 -0300

    UPDATE: requirement 5 implemented

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (!animal) {
+    return species.reduce((animals, specie) =>
+      ({ ...animals, [specie.name]: specie.residents.length }), {});
+  }
+  if (Object.keys(animal).includes('sex')) {
+    return species.find(({ name }) => name === animal.specie).residents
+      .filter(({ sex }) => sex === animal.sex).length;
+  }
 
+  return species.find(({ name }) => name === animal.specie).residents.length;
+}
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
