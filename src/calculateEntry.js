0918d742871fcca9f181cd9fb0dee7def84bc377
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-30 15:47:25 -0300

    UPDATE: requirement 8 implemented

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,32 +1,28 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  entrants.reduce((peoples, { age }) => {
-    console.log(peoples, age);
-    if (!peoples[age]) {
-      peoples[age] = 1;
-    } else if (peoples.age < 18) {
-      peoples[age] += 1;
-    } else if (peoples.age >= 18 && peoples.age < 50) {
-      peoples[age] += 1;
-    } else {
-      peoples[age] += 1;
+  return entrants.reduce((acc, { age }) => {
+    if (age < 18) {
+      acc.child += 1;
     }
-    return peoples;
-  }, {});
+    if (age >= 18 && age < 50) {
+      acc.adult += 1;
+    }
+    if (age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) {
     return 0;
   }
+  return (countEntrants(entrants).adult * prices.adult)
+    + (countEntrants(entrants).child * prices.child)
+    + (countEntrants(entrants).senior * prices.senior);
 }
-console.log(countEntrants([
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-]));
+
 module.exports = { calculateEntry, countEntrants };
