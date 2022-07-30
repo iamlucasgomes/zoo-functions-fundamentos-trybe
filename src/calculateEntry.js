98c3afae3ec886c631a35217fb00641348fc207c
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-30 01:11:58 -0300

    UPDATE: requirement 8 initiated

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aquii
+  entrants.reduce((peoples, { age }) => {
+    console.log(peoples, age);
+    if (!peoples[age]) {
+      peoples[age] = 1;
+    } else if (peoples.age < 18) {
+      peoples[age] += 1;
+    } else if (peoples.age >= 18 && peoples.age < 50) {
+      peoples[age] += 1;
+    } else {
+      peoples[age] += 1;
+    }
+    return peoples;
+  }, {});
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
 }
-
+console.log(countEntrants([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
 module.exports = { calculateEntry, countEntrants };
