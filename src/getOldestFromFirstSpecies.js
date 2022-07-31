16fb017bdaa901126aa013b6c41711b397e9db98
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-31 17:41:47 -0300

    UPDATE: requirement 10 implemented

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findAnimal = employees.find(({ id: ids }) => id === ids).responsibleFor
+    .find((firstAnimal) => firstAnimal);
+  const names = species.filter(({ id: ids }) => ids === findAnimal)
+    .find(({ residents }) => residents).residents.sort((a, b) => b.age - a.age)
+    .find((animal) => animal).name;
+  const sexs = species.filter(({ id: ids }) => ids === findAnimal)
+    .find(({ residents }) => residents).residents.sort((a, b) => b.age - a.age)
+    .find((animal) => animal).sex;
+  const ages = species.filter(({ id: ids }) => ids === findAnimal)
+    .find(({ residents }) => residents).residents.sort((a, b) => b.age - a.age)
+    .find((animal) => animal).age;
+  return Object.values({ names, sexs, ages });
 }
 
 module.exports = getOldestFromFirstSpecies;
