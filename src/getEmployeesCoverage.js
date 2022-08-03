99e7fd0119b6246a3d726749cbd5535827b3f559
iamlucasgomes <lucas.devjs@gmail.com>
2022-08-02 23:22:39 -0300

    UPDATE: avanços requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,26 +1,35 @@
 const data = require('../data/zoo_data');
 const { employees, species } = require('../data/zoo_data');
 
+const verification = (objeto) => employees.map(({ firstName }) => firstName).concat(employees
+  .map(({ lastName }) => lastName)).includes(objeto.name);
+
+const findName = (objeto) => employees.find(({ firstName, lastName }) => firstName === objeto.name
+  || lastName === objeto.name);
+
 function getEmployeesCoverage(objeto) {
-  if (!objeto) {
-    return employees.filter(({ firstName, lastName }) => `${firstName} ${lastName}`)
-      .map(({ id, firstName, lastName, responsibleFor }) => ({
-        id,
-        fullName: `${firstName} ${lastName}`,
-        responsibleFor,
-        locations: species.find(({ location }) => location).location,
-      }));
+  if (!verification(objeto)) {
+    throw new Error('Informações inválidas');
   }
-  // if (employees.filter(({ id }) => id.includes(objeto.name))) {
-  //   return employees.filter(({ id }) => id);
-  // }
-  // if (employees.filter(({ firstName, lastName }) => firstName.includes(objeto.name)
-  //   || lastName.includes(objeto.name))) {
-  //   return employees.filter(({ firstName, lastName }) => `${firstName} ${lastName}`);
-  // }
+  if (verification(objeto)) {
+    return {
+      id: findName(objeto).id,
+      fullName: `${findName(objeto).firstName} ${findName(objeto).lastName}`,
+      species: findName(objeto).responsibleFor.reduce((arr, element) => {
+        arr.push(species.find(({ id }) => id === element).name);
+        return arr;
+      }, []),
+      locations: findName(objeto).responsibleFor.reduce((arr, element) => {
+        arr.push(species.find(({ id }) => id === element).location);
+        return arr;
+      }, []),
+    };
+  }
 }
+
 // console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
-// console.log(getEmployeesCoverage({ name: 'Spry' }));
+console.log(getEmployeesCoverage({ name: 'Spry' }));
 // console.log(getEmployeesCoverage({ id: 'Id inválido' }));
-console.log(getEmployeesCoverage());
+// console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
+// console.log(species.filter(({ id }) => id));
